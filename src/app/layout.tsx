fa46dc0fa3ee69a2953ec03ad9fad634d71175d8
CTRL <CTRL>
2026-02-22 18:46:07 +0530

    Add Tawk.to chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         <main>{children}</main>
         <Footer />
+        {/* Tawk.to chat widget */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/699b01252c4f881c3a336e7c/1ji2novd4';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();`,
+          }}
+        />
       </body>
     </html>
   )
