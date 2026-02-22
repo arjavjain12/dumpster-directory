0991c3981f03f0a83e695499f0abb471906a6372
CTRL <CTRL>
2026-02-22 18:49:18 +0530

    Fix Tawk.to: use next/script with afterInteractive strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
@@ -82,7 +83,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main>{children}</main>
         <Footer />
         {/* Tawk.to chat widget */}
-        <script
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/699b01252c4f881c3a336e7c/1ji2novd4';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();`,
           }}
